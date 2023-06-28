0ad0f378a3365d95d6765f7062090977746e044e
JiungChoi <wldnd6091@gmail.com>
2023-06-29 00:28:09 +0900

    COMMIT

--- baekjoon_11725.py
+++ baekjoon_11725.py
@@ -1,4 +1,4 @@
-# 트리의 부모찾기 : DFS
+# 트리의 부모찾기 : DFS, 스택
 from sys import stdin
 
 N = int(stdin.readline())
@@ -14,14 +14,12 @@ def whoIsParent(graph):
             if not haveParent[nextVertex]:
                 haveParent[nextVertex] = nowVertex
                 queue.append(nextVertex)
-
-                
+ 
 
 for i in range(1, N):
     a, b = map(int, stdin.readline().split())
     graph[a].append(b)
     graph[b].append(a)
-
 
 whoIsParent(graph)
 print("\n".join(map(str, haveParent[2:])))
